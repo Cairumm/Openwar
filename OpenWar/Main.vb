c9eada869f1ae62c05cdd4d70fb30b433b4dad4e
Duane Roelands <duane.roelands@gmail.com>
2012-08-30 16:35:55 -0400

    Cleaning up
    grouping methods together

--- OpenWar/Main.vb
+++ OpenWar/Main.vb
@@ -65,9 +65,49 @@
 
     End Sub
 
+    Private Sub Main_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+        Me.CurrentMousePosition.X = e.X
+        Me.CurrentMousePosition.Y = e.Y
+        Me.Invalidate()
+    End Sub
+
+    Private Function SetStartingYear() As Integer
+        Dim rng As New Random(System.DateTime.Now.Millisecond)
+        Return rng.Next(1956, 1965)
+    End Function
+
+    Public Sub PeaceTurnButtons_Show()
+        Me.btnBuildBase.Visible = True
+        Me.btnDeclareWar.Visible = True
+        Me.btnSpy.Visible = True
+    End Sub
+
+    Public Sub PeaceTurnButtons_Hide()
+        Me.btnBuildBase.Visible = False
+        Me.btnDeclareWar.Visible = False
+        Me.btnSpy.Visible = False
+    End Sub
+
+    Public Sub BaseTypeButtons_Show()
+        Me.btnBuildABMBase.Visible = True
+        Me.btnBuildBomberBase.Visible = True
+        Me.btnBuildMissileBase.Visible = True
+        Me.btnBuildSubBase.Visible = True
+    End Sub
+
+    Public Sub BaseTypeButtons_Hide()
+        Me.btnBuildABMBase.Visible = False
+        Me.btnBuildBomberBase.Visible = False
+        Me.btnBuildMissileBase.Visible = False
+        Me.btnBuildSubBase.Visible = False
+    End Sub
+
+    Private Sub SetMessage(Message As String)
+        Me.txtMessage.Text = Message
+    End Sub
 
 
-#Region "Map Coordinate Generation"
+#Region "Map Routines"
     Private Sub GenerateHexPoints()
         Dim RowNumber As Integer = 1
         GenerateHexRow(1, RowNumber)
@@ -112,7 +152,6 @@
 
         RowNumber += 1
     End Sub
-#End Region
 
     Public Sub SetupMaps()
         For x = 0 To 101
@@ -166,70 +205,73 @@
         ' We need to assign rows and columns to each Map tile so that we can know which hexes are next to each other
         ' in case a missile goes off target
         For x = 0 To 101
+            Dim ThisTile As MapTile = Map(x)
+
             ' set Map rows for each tile
             Select Case x
                 Case 0, 2, 5, 9, 14, 20
-                    Map(x).MapRow = 0
+                    ThisTile.MapRow = 0
                 Case 1, 4, 8, 13, 19, 25, 31
-                    Map(x).MapRow = 1
+                    ThisTile.MapRow = 1
                 Case 3, 7, 12, 18, 24, 30, 36, 42
-                    Map(x).MapRow = 2
+                    ThisTile.MapRow = 2
                 Case 6, 11, 17, 23, 29, 35, 41, 47, 53
-                    Map(x).MapRow = 3
+                    ThisTile.MapRow = 3
                 Case 10, 16, 22, 28, 34, 40, 46, 52, 58, 64
-                    Map(x).MapRow = 4
+                    ThisTile.MapRow = 4
                 Case 15, 21, 27, 33, 39, 45, 51, 57, 63, 69, 75
-                    Map(x).MapRow = 5
+                    ThisTile.MapRow = 5
                 Case 26, 32, 38, 44, 50, 56, 62, 68, 74, 80, 86
-                    Map(x).MapRow = 6
+                    ThisTile.MapRow = 6
                 Case 37, 43, 49, 55, 61, 67, 73, 79, 85, 91
-                    Map(x).MapRow = 7
+                    ThisTile.MapRow = 7
                 Case 48, 54, 60, 66, 72, 78, 84, 90, 95
-                    Map(x).MapRow = 8
+                    ThisTile.MapRow = 8
                 Case 59, 65, 71, 77, 83, 89, 94, 98
-                    Map(x).MapRow = 9
+                    ThisTile.MapRow = 9
                 Case 70, 76, 82, 88, 93, 97, 100
-                    Map(x).MapRow = 10
+                    ThisTile.MapRow = 10
                 Case 81, 87, 92, 96, 99, 101
-                    Map(x).MapRow = 11
+                    ThisTile.MapRow = 11
             End Select
 
             ' set Map columns for each tile
             Select Case x
                 Case 15, 26, 37, 48, 59, 70, 81
-                    Map(x).MapColumn = 0
+                    ThisTile.MapColumn = 0
                 Case 10, 21, 32, 43, 54, 65, 76, 87
-                    Map(x).MapColumn = 1
+                    ThisTile.MapColumn = 1
                 Case 6, 16, 27, 38, 49, 60, 71, 82, 92
-                    Map(x).MapColumn = 2
+                    ThisTile.MapColumn = 2
                 Case 3, 11, 22, 33, 44, 55, 66, 77, 88, 96
-                    Map(x).MapColumn = 3
+                    ThisTile.MapColumn = 3
                 Case 1, 7, 17, 28, 39, 50, 61, 72, 83, 93, 99
-                    Map(x).MapColumn = 4
+                    ThisTile.MapColumn = 4
                 Case 0, 4, 12, 23, 34, 45, 56, 67, 78, 89, 97, 101
-                    Map(x).MapColumn = 5
+                    ThisTile.MapColumn = 5
                 Case 2, 8, 18, 29, 40, 51, 62, 73, 84, 94, 100
-                    Map(x).MapColumn = 6
+                    ThisTile.MapColumn = 6
                 Case 5, 13, 24, 35, 46, 57, 68, 79, 90, 98
-                    Map(x).MapColumn = 7
+                    ThisTile.MapColumn = 7
                 Case 9, 19, 30, 41, 52, 63, 74, 85, 95
-                    Map(x).MapColumn = 8
+                    ThisTile.MapColumn = 8
                 Case 14, 25, 36, 47, 58, 69, 80, 91
-                    Map(x).MapColumn = 9
+                    ThisTile.MapColumn = 9
                 Case 20, 31, 42, 53, 64, 75, 86
-                    Map(x).MapColumn = 10
+                    ThisTile.MapColumn = 10
             End Select
 
             'mark coastal tiles
             'we need to know which tiles are coastal to determine whether or not a sub base can be built there
             Select Case x
                 Case 15, 10, 6, 3, 1, 0, 2, 5, 9, 14, 20, 26, 37, 48, 59, 70, 81, 31, 42, 53, 64, 75, 86, 87, 92, 96, 99, 101, 100, 98, 95, 91
-                    Map(x).IsCoastal = True
+                    ThisTile.IsCoastal = True
                 Case Else
-                    Map(x).IsCoastal = False
+                    ThisTile.IsCoastal = False
             End Select
 
-            Map(x).TileType = MapTile.MapTileType.Empty
+            ThisTile.TileType = MapTile.MapTileType.Empty
+            ThisTile.IsVisibleToOpponent = True
         Next
     End Sub
 
@@ -263,17 +305,7 @@
         Next
         Return ReturnValue
     End Function
-
-    Private Sub Main_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
-        Me.CurrentMousePosition.X = e.X
-        Me.CurrentMousePosition.Y = e.Y
-        Me.Invalidate()
-    End Sub
-
-    Private Function SetStartingYear() As Integer
-        Dim rng As New Random(System.DateTime.Now.Millisecond)
-        Return rng.Next(1956, 1965)
-    End Function
+#End Region
 
 #Region "Graphics/Drawing"
     Private Sub Main_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
@@ -397,36 +429,5 @@
         End If
     End Function
 #End Region
-
-
-    Public Sub PeaceTurnButtons_Show()
-        Me.btnBuildBase.Visible = True
-        Me.btnDeclareWar.Visible = True
-        Me.btnSpy.Visible = True
-    End Sub
-
-    Public Sub PeaceTurnButtons_Hide()
-        Me.btnBuildBase.Visible = False
-        Me.btnDeclareWar.Visible = False
-        Me.btnSpy.Visible = False
-    End Sub
-
-    Public Sub BaseTypeButtons_Show()
-        Me.btnBuildABMBase.Visible = True
-        Me.btnBuildBomberBase.Visible = True
-        Me.btnBuildMissileBase.Visible = True
-        Me.btnBuildSubBase.Visible = True
-    End Sub
-
-    Public Sub BaseTypeButtons_Hide()
-        Me.btnBuildABMBase.Visible = False
-        Me.btnBuildBomberBase.Visible = False
-        Me.btnBuildMissileBase.Visible = False
-        Me.btnBuildSubBase.Visible = False
-    End Sub
-
-    Private Sub SetMessage(Message As String)
-        Me.txtMessage.Text = Message
-    End Sub
 
 End Class
